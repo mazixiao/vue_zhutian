8c926bd7789b1701cfe492665a9de9a49ab28d92
mazixiao <you@example.com>
2018-11-15 18:25:02 +0800

    测试

--- src/components/header.vue
+++ src/components/header.vue
@@ -46,7 +46,7 @@
   <div class="right_share">
     <a class="item phone" href="tel:84799023">
         <i class="iconfont icon-dianhua1"></i>
-        <span>84799023</span>
+        <span>000000</span>
     </a>
     <a class="item qq" target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=1931104552&amp;site=qq&amp;menu=yes">
         <i class="icon iconfont icon-QQ"></i>
